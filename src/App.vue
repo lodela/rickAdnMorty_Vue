76541c538219d9c17abf1da64503620caceffd4e
norberto lodela <lodela.n@gmail.com>
2022-11-27 15:34:35 -0600

    get personajes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Rick And Morty</h1>
+  <ListCharacters />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListCharacters from '@/components/ListCharacters';
 
 export default {
-  name: 'App',
+  name: 'Norberto Lodela',
   components: {
-    HelloWorld
-  }
+    ListCharacters
+}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,31 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    characters: [],
+    charactersFilter: [],
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    setCharacters(state, payload) {
+      state.characters = payload;
+    },
+    setCharactersFilter(state, payload) {
+      state.charactersFilter = payload;
+    },
   },
   actions: {
+    async getCharacters({ commit }) {
+      const options = { method: "GET" };
+
+      fetch("https://rickandmortyapi.com/api/character", options)
+        .then((response) => response.json())
+        .then((response) => {
+          commit("setCharacters", response.results);
+          commit("setCharactersFilter", response.results);
+        })
+        .catch((err) => console.error(err));
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
